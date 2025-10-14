b53a947d43053146a6d6a35c7772e377f9d7cf2c
HUNGNAMHOMEPC\truon <truongnhuhungnam@gmail.com>
2025-10-14 10:57:11 +0700

    Refactor CareerTimeline component: removed unused imports and streamlined code for better readability.

--- src/components/CareerSection/CareerTimeline.tsx
+++ src/components/CareerSection/CareerTimeline.tsx
@@ -1,5 +1,5 @@
+import { Award, Users } from "lucide-react"
 import { ScrollTimeline } from "../lightswind/scroll-timeline"
-import { Briefcase, Award, Layers, Users, Globe } from "lucide-react"
 
 export const CareerTimeline = () => {
   const careerEvents = [
